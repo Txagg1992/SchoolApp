ab25e441e6d98114cf9e2066a1251047187fd546
Enoch Phetteplace <curiousaps@gmail.com>
2020-05-19 14:31:12 -0500

    Initial Commit. Build Application. Add Base Activity.

---
+++ app/src/main/java/com/curiousapps/schoolapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.curiousapps.schoolapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mProgressBar.getVisibility() == View.VISIBLE){
+                    showProgressBar(false);
+                }else {
+                    showProgressBar(true);
+                }
+            }
+        });
+    }
+}
